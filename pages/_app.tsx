7a834a7e7d0d2393e0c1dfcc53240b4511da14f9
John Paul Onte <jnpl.onte@gmail.com>
2022-11-16 16:49:50 +0800

    update contract

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from 'next/app';
 
 import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { ToastContainer } from 'react-toastify';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -20,6 +22,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 				}}
 			>
 				<NextUIProvider>
+					<ToastContainer
+						position="top-right"
+						autoClose={5000}
+						hideProgressBar={true}
+						newestOnTop={false}
+						closeOnClick
+						rtl={false}
+						pauseOnFocusLoss
+						draggable
+						pauseOnHover
+						theme="dark"
+					/>
 					<Component className="container" {...pageProps} />
 				</NextUIProvider>
 			</NextThemesProvider>
--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 
-import { Container, Card, Text, Grid, Input, Spacer, Button } from '@nextui-org/react';
+import { Container, Card, Text, Grid, Input, Spacer, Button, Loading } from '@nextui-org/react';
+import { toast } from 'react-toastify';
 
 import Head from 'next/head';
 
@@ -20,11 +21,12 @@ import { MainLayout } from '../../layouts';
 
 import styles from '../../styles/game.module.css';
 
+const $$client = Client.forTestnet();
 const Game: NextPage = () => {
-	const $client = Client.forTestnet();
-
 	const [$loading, $setLoading] = useState(true);
+	const [$contractLoading, $setContractLoading] = useState(true);
 	const [$playerLimit, $setPlayerLimit] = useState<number>(0);
+	const [$balance, $setBalance] = useState<number>(0);
 	const [$price, $setPrice] = useState<number>(0);
 	const [$address, $setAddress] = useState<string>('');
 	// const [$referalAddress, $setReferalAddress] = useState<string>('');
@@ -40,8 +42,8 @@ const Game: NextPage = () => {
 			.setGas(3000000)
 			.setFunction('getPrice');
 
-		const priceResponse = await priceTransaction.execute($client);
-		const xpriceResponse = await priceResponse.getRecord($client);
+		const priceResponse = await priceTransaction.execute($$client);
+		const xpriceResponse = await priceResponse.getRecord($$client);
 		const xxpriceResponse = await xpriceResponse.contractFunctionResult;
 		if (xxpriceResponse) {
 			$setPrice(Number(xxpriceResponse.getUint256(0)));
@@ -52,20 +54,41 @@ const Game: NextPage = () => {
 			.setGas(3000000)
 			.setFunction('getPlayerLimit');
 
-		const playerLimitResponse = await playerLimitTransaction.execute($client);
-		const xplayerLimitResponse = await playerLimitResponse.getRecord($client);
+		const playerLimitResponse = await playerLimitTransaction.execute($$client);
+		const xplayerLimitResponse = await playerLimitResponse.getRecord($$client);
 		const xxplayerLimitResponse = await xplayerLimitResponse.contractFunctionResult;
 		if (xxplayerLimitResponse) {
 			$setPlayerLimit(Number(xxplayerLimitResponse.getUint256(0)));
 		}
 	};
 
-	const getBalance = async (_myAccountId: any) => {
-		const accountBalance = await new AccountBalanceQuery().setAccountId(_myAccountId).execute($client);
-		console.log('balance', Number(accountBalance.hbars.toTinybars()) / 100000000);
+	const getBalance = async () => {
+		if ($address) {
+			const accountBalance = await new AccountBalanceQuery().setAccountId($address).execute($$client);
+			$setBalance(Number(accountBalance.hbars.toTinybars()) / 100000000);
+		}
 	};
 
+	// const startGane = async () => {
+	// 	const contractId: string = ContractId.fromString(process.env.NEXT_PUBLIC_CONTRACT_ID || '').toString();
+	// 	const playerTransaction = new ContractExecuteTransaction()
+	// 		.setContractId(contractId)
+	// 		.setGas(300000)
+	// 		.setFunction('startGame');
+
+	// 	const playerLimitResponse = await playerTransaction.execute($$client);
+
+	// 	//Request the receipt of the transaction
+	// 	const receipt = await playerLimitResponse.getReceipt($$client);
+	// 	//Get the transaction consensus status
+	// 	const transactionStatus = receipt.status;
+
+	// 	console.log('The transaction consensus status is ' + transactionStatus);
+	// };
+
 	const joinGame = async () => {
+		$setContractLoading(true);
+
 		const contractId: string = ContractId.fromString(process.env.NEXT_PUBLIC_CONTRACT_ID || '').toString();
 		const param = new ContractFunctionParameters();
 		param.addAddress('0000000000000000000000000000000002E80425');
@@ -75,27 +98,37 @@ const Game: NextPage = () => {
 			.setPayableAmount(new Hbar(100))
 			.setFunction('setPlayerData', param);
 
-		const playerLimitResponse = await playerTransaction.execute($client);
-		const receipt = await playerLimitResponse.getReceipt($client);
-		const transactionStatus = receipt.status;
+		const playerLimitResponse = await playerTransaction.execute($$client);
 
-		console.log('The transaction status is ', transactionStatus);
+		//Request the receipt of the transaction
+		const receipt = await playerLimitResponse.getReceipt($$client);
+		console.log(receipt, 'receiptreceiptreceipt');
+		if (receipt.status) {
+			toast('JOIN SUCCESS');
+			getBalance();
+		}
+
+		$setContractLoading(false);
 	};
 
 	useEffect(() => {
 		const myPrivateKey = PrivateKey.fromString(process.env.NEXT_PUBLIC_TEST_PRIVATE || '');
 		const myAccountId: string = AccountId.fromString(process.env.NEXT_PUBLIC_TEST_ACCOUNT || '').toString();
 		if (myAccountId && myPrivateKey) {
-			$client.setOperator(myAccountId, myPrivateKey);
+			$$client.setOperator(myAccountId, myPrivateKey);
 			$setAddress(myAccountId);
-
-			getBalance(myAccountId);
-			getInitData();
 		}
 
 		$setLoading(false);
+		$setContractLoading(false);
 		// eslint-disable-next-line
 	}, []);
+
+	useEffect(() => {
+		getBalance();
+		getInitData();
+		// eslint-disable-next-line
+	}, [$address]);
 
 	return (
 		<MainLayout>
@@ -112,9 +145,14 @@ const Game: NextPage = () => {
 					<Container className="container">
 						<Card className="card">
 							<Card.Body>
+								<Text> - BALANCE: {$balance}</Text>
 								<Text> - PRICE: {$price}</Text>
 								<Text> - PLAYER LIMIT: {$playerLimit}</Text>
 								<Text> - NUMBER OF PLAYERS: 0</Text>
+								<Spacer y={1} />
+								{/* <Button size="md" className="full_width" auto onPress={startGane}>
+									START GAME
+								</Button> */}
 								<Spacer y={1} />
 								<div className="form">
 									<Spacer y={1} />
@@ -137,9 +175,15 @@ const Game: NextPage = () => {
 									<Spacer y={1} />
 									<Grid.Container gap={2}>
 										<Grid xs={12} md={12}>
-											<Button size="md" className="full_width" auto onPress={joinGame}>
-												JOIN GAME
-											</Button>
+											{$contractLoading ? (
+												<div className="full_width text_center">
+													<Loading type="points" size="xl" />
+												</div>
+											) : (
+												<Button size="md" className="full_width" auto onPress={joinGame}>
+													JOIN GAME
+												</Button>
+											)}
 										</Grid>
 									</Grid.Container>
 								</div>
